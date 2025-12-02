647dd108a3af6f791f17c54d32f561dfd5402467
tanickell <tnickell1@cnm.edu>
2025-12-02 08:43:50 -0700

    Second commit (testing).

--- Catblocks/src/Catblocks.cpp
+++ Catblocks/src/Catblocks.cpp
@@ -13,13 +13,6 @@
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
-// Run the application and system concurrently in separate threads
-SYSTEM_THREAD(ENABLED);
-
-// Show system, cloud connectivity, and application logs over USB
-// View logs with CLI using 'particle serial monitor --follow'
-SerialLogHandler logHandler(LOG_LEVEL_INFO);
-
 // setup() runs once, when the device is first turned on
 void setup() {
   // Put initialization like pinMode and begin functions here
